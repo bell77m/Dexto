ec3158822f22246c7d4ec227cba56de4fdb016b5
bell77m <bell77m@gmail.com>
2025-01-30 11:27:00 +0700

    Initial commit ⚡️

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,7 +4,7 @@ import type { DocumentHead } from "@builder.io/qwik-city";
 export default component$(() => {
   return (
     <>
-      <h1>Hi 👋</h1>
+      <h1>Hi, Hello World👋</h1>
       <div>
         Can't wait to see what you build with qwik!
         <br />
