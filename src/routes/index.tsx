73cbae980a3880fea46ffee33ac72b53beb80335
bell77m <bell77m@gmail.com>
2025-01-31 11:04:56 +0700

    Initial commit ⚡️
    Signed-off-by: bell77m <bell77m@gmail.com>

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,18 +3,16 @@ import type { DocumentHead } from "@builder.io/qwik-city";
 
 export default component$(() => {
   return (
-    <>
-      <h1>Hi  Hello👋</h1>
-      <div>
-        Can't wait to see what you build with qwik!
-        <br />
-        Happy coding.
-      </div>
-    </>
+      <>
+        <h1>Hi 👋</h1>
+        <div>
+          Can't wait to see what you build with qwik!
+          <br />
+          Happy coding.
+        </div>
+      </>
   );
 });
-
-
 
 export const head: DocumentHead = {
   title: "Welcome to Qwik",
