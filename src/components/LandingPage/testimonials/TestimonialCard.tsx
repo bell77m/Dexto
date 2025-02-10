ada8a32d2cd1bc218dae6ba7fcd2480cbbb61188
deztyy <tibet2546@gmail.com>
2025-02-10 16:26:04 +0700

    Modify Testimonial

--- src/components/LandingPage/testimonials/TestimonialCard.tsx
+++ src/components/LandingPage/testimonials/TestimonialCard.tsx
@@ -3,20 +3,20 @@ import type { TestimonialProps } from "./types";
 
 export const TestimonialCard = component$((props: TestimonialProps) => {
   return (
-    <div class="flex flex-col items-start pt-6 pr-0.5 pb-36 pl-12 mx-auto w-full text-white bg-gray-800 rounded-xl max-md:pb-24 max-md:pl-5 max-md:mt-5">
-      <div class="flex gap-9">
+    <div class="flex flex-col items-start pt-6 pr-0.5  pl-12 mx-auto w-full text-white bg-gray-800 rounded-xl max-md:pb-24 max-md:pl-5 max-md:mt-5">
+      <div class="flex gap-9 ">
         <img
           loading="lazy"
           src={props.imageSrc}
           alt={`Profile picture of ${props.name}`}
           class="object-contain shrink-0 max-w-full rounded-full aspect-[1.03] w-[106px]"
         />
-        <div class="flex flex-col self-start mt-3.5">
+        <div class="flex flex-col self-start mt-3.5 max-w-[280px]">
           <div class="text-xl">{props.name}</div>
           <div class="mt-2 text-base font-light">{props.role}</div>
         </div>
       </div>
-      <div class="mt-6 text-base">{props.content}</div>
+      <div class="mt-6  flex-col max-w-[450px] h-[100px]  m-16">{props.content}</div>
     </div>
   );
 });
--- src/components/LandingPage/testimonials/TestimonialsSection.tsx
+++ src/components/LandingPage/testimonials/TestimonialsSection.tsx
@@ -6,8 +6,8 @@ const testimonials: TestimonialProps[] = [
   {
     name: "Chawakorn Chayakoom",
     role: "Co-Founder PonHab",
-    content: "From my experience I have been using this website for like 3 month it is a very great choice when you work with your team",
-    imageSrc: "https://cdn.builder.io/api/v1/image/assets/TEMP/4d2e926641666a8396876e6c7df746d9acdbdec831b2229cdb4ba898b927a1b3?placeholderIfAbsent=true&apiKey=83086b8ef5ae4f9392edc247fd20d152"
+    content: "From my experience I have been using this website for like 3 month it is a very great choice when you work with your team i love you so much because im so gay right now",
+    imageSrc: "https://cdn.builder.io/api/v1/image/assets/TEMP/df518153a15c9d006ada4e9d2aaede04c94a5d1b311591da5331b5cdd1dcd5a9?placeholderIfAbsent=true&apiKey=83086b8ef5ae4f9392edc247fd20d152"
   },
   {
     name: "Sasikhet M",
@@ -17,6 +17,18 @@ const testimonials: TestimonialProps[] = [
   },
   {
     name: "Lobute Gulliman",
+    role: "Co-Founder PonHab",
+    content: "From my experience I have been using this website for like 3 month it is a very great choice when you work with your team",
+    imageSrc: "https://cdn.builder.io/api/v1/image/assets/TEMP/ec354c4db3755da3a8295b7475afe60174e94698c0958011590e1524485c2325?placeholderIfAbsent=true&apiKey=83086b8ef5ae4f9392edc247fd20d152"
+  },
+  {
+    name: "Sasikhet Gaylord",
+    role: "Co-Founder PonHab",
+    content: "From my experience I have been using this website for like 3 month it is a very great choice when you work with your team",
+    imageSrc: "https://cdn.builder.io/api/v1/image/assets/TEMP/ec354c4db3755da3a8295b7475afe60174e94698c0958011590e1524485c2325?placeholderIfAbsent=true&apiKey=83086b8ef5ae4f9392edc247fd20d152"
+  },
+  {
+    name: "Aik sud lor",
     role: "Co-Founder PonHab",
     content: "From my experience I have been using this website for like 3 month it is a very great choice when you work with your team",
     imageSrc: "https://cdn.builder.io/api/v1/image/assets/TEMP/ec354c4db3755da3a8295b7475afe60174e94698c0958011590e1524485c2325?placeholderIfAbsent=true&apiKey=83086b8ef5ae4f9392edc247fd20d152"
@@ -32,15 +44,15 @@ export const TestimonialsSection = component$(() => {
       <div class="mt-9 text-4xl font-semibold text-center text-white max-md:max-w-full">
         What Our Customers Are Saying
       </div>
-      <div class="overflow-hidden self-stretch mt-20 mb-0 w-full max-md:mt-10 max-md:mb-2.5 max-md:max-w-full">
-        <div class="flex gap-5 max-md:flex-col">
-          {testimonials.map((testimonial, index) => (
-            <div key={index} class="flex flex-col w-1/3 max-md:ml-0 max-md:w-full">
-              <TestimonialCard {...testimonial} />
-            </div>
-          ))}
+      <div class="overflow-hidden mt-20 w-full overflow-x-auto scroll-smooth snap-x snap-mandatory ">
+      <div class="flex gap-5 flex-nowrap w-max bg-gree">
+            {testimonials.map((testimonial, index) => (
+              <div key={index} class="flex flex-col w-1/3 max-md:ml-0 max-md:w-full snap-center">
+                <TestimonialCard {...testimonial} />
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
     </div>
   );
 });
