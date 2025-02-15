bcd0f5929983215f933f1aec18ccc8c3e5a5112b
deztyy <tibet2546@gmail.com>
2025-02-15 22:11:43 +0700

    Modify home page

---
+++ src/routes/home/index.tsx
@@ -0,0 +1,11 @@
+import { component$ } from '@builder.io/qwik';
+import { Sidebar } from '~/components/sidebar/Sidebar';
+
+export default component$(() => {
+  return (
+    <>
+      <Sidebar/>
+    </>
+  );
+});
+
