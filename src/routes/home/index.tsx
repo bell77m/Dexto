d5fe94c011bea96542fb546d87bbe8cabfc06201
deztyy <tibet2546@gmail.com>
2025-02-17 14:28:49 +0700

    modify

--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -9,7 +9,6 @@
 //   );
 // });
 import { component$ } from '@builder.io/qwik';
-//import { MessageSection } from '~/components/creamm/MessageSection';
 import { Sidebar } from '~/components/sidebar/Sidebar';
 
 export default component$(() => {
