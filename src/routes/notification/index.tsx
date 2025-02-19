e8d5dbcbbe0ecdda05fff96ead7d724a3db58a9a
IIMileII <milechawakorn@gmail.com>
2025-02-19 23:04:03 +0700

    Add Notification

--- src/routes/notification/index.tsx
+++ src/routes/notification/index.tsx
@@ -1,7 +1,46 @@
 import { component$ } from '@builder.io/qwik';
+import { Notification } from '~/components/notification/notification';
 import { Sidebar } from '~/components/sidebar/Sidebar';
 
 export default component$(() => {
+  const notiData = [
+    {
+      id: 1,
+      title: "Project Invite",
+      description: "Sommeone has invite you to project",
+      image1: "/image/project.svg",
+      image2: "/image/minnie.svg"
+    },
+    {
+      id: 2,
+      title: "Friend Request",
+      description: "Sommeone want to be your friend",
+      image1: "/image/add-friend.svg",
+      image2: "/image/donaldduck.svg"
+    },
+    {
+      id: 3,
+      title: "Project Invite",
+      description: "Sommeone has invite you to project",
+      image1: "/image/project.svg",
+      image2: "/image/minnie.svg"
+    },
+    {
+      id: 4,
+      title: "Friend Request",
+      description: "Sommeone want to be your friend",
+      image1: "/image/add-friend.svg",
+      image2: "/image/donaldduck.svg"
+    },
+    {
+      id: 5,
+      title: "Project Invite",
+      description: "Sommeone has invite you to project",
+      image1: "/image/project.svg",
+      image2: "/image/minnie.svg"
+    }
+  ]
+
   return (
     <>
       <div class="flex h-screen">
@@ -14,6 +53,11 @@ export default component$(() => {
             </h1>
           </div>
           <div class="w-full h-px mt-4 bg-gray-400"></div>
+          <div class="max-w-[1200px] h-full ml-32 mt-7 p-3 overflow-y-auto scroll-smooth">
+          {notiData.map((item) => (
+            <Notification key={item.id} {...item} />
+          ))}
+          </div>
         </section>
       </div>
     </>
