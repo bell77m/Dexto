c704f5c0a389e6f73a0faedc33c4d1e9b19429a8
creamz01 <chonnikan9806@gmail.com>
2025-03-09 00:34:21 +0700

    เพิ่มหน้า project

---
+++ src/routes/project/index.tsx
@@ -0,0 +1,14 @@
+import { component$ } from '@builder.io/qwik';
+import { MyProject } from '~/components/Project/MyProject';
+import { Sidebar } from '~/components/sidebar/Sidebar';
+
+export default component$(() => {
+  return (
+    <>
+    <div class="flex h-screen">
+      <Sidebar/>
+      <MyProject class="flex-grow"/>
+    </div>
+    </>
+  );
+});
