d7ffd9751f74cd83fb4d770c6ddc4cebfdeca886
Domtaoz <domdypol@gmail.com>
2025-03-13 14:30:28 +0700

    modify forum URL

--- src/components/forum/ForumContent.tsx
+++ src/components/forum/ForumContent.tsx
@@ -1,5 +1,7 @@
 import { component$, useSignal, $, useVisibleTask$ } from "@builder.io/qwik";
 import { useUserStore } from "~/store/store";
+import API_URL from "~/configURL/config";
+
 
 export default component$(() => {
   const { userId } = useUserStore();
@@ -10,7 +12,7 @@ export default component$(() => {
   const loadPosts = $(async () => {
     isLoading.value = true;
     try {
-      const res = await fetch("http://dexto.com:3000/graphql", {
+      const res = await fetch(API_URL, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
@@ -57,7 +59,7 @@ export default component$(() => {
     const commentText = newComment.value[postId]?.trim();
     if (!commentText || commentText.length === 0) return alert("⚠️ Comment cannot be empty!");
   
-    await fetch("http://dexto.com:3000/graphql", {
+    await fetch(API_URL, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
@@ -81,7 +83,7 @@ export default component$(() => {
 
   const deletePost = $(async (postId: number) => {
     if (!confirm("Are you sure you want to delete this post?")) return;
-    const response = await fetch("http://dexto.com:3000/graphql", {
+    const response = await fetch(API_URL, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
