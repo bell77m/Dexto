b51f5796b804c6f0cdd8bc228008ba8ad93aa0cd
IIMileII <milechawakorn@gmail.com>
2025-03-14 07:36:21 +0700

    modify chat index

--- src/routes/chat/index.tsx
+++ src/routes/chat/index.tsx
@@ -5,8 +5,8 @@ import { Sidebar } from '~/components/sidebar/Sidebar';
 export default component$(() => {
   return (
     <div class="flex h-screen bg-gray-700">
-      <Sidebar /> {/* ✅ Sidebar หลัก */}
-      <ChatMain /> {/* ✅ หน้าต่างแชทหลัก */}
+      <Sidebar /> {/*  Sidebar หลัก */}
+      <ChatMain /> {/*  หน้าต่างแชทหลัก */}
     </div>
   );
 });
